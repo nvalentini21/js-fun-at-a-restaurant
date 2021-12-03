5df67be55d723070100379c9a425d3e7e6586562
Nicole Valentini <ncvalentini@gmail.com>
2021-12-02 21:03:04 -0700

    Completed meal tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,43 @@
-function nameMenuItem() {
+function nameMenuItem(menuItemName) {
+  return `Delicious ${menuItemName}`;
+};
 
+function createMenuItem(name, price, type){
+  var foodItem = {
+    name: name,
+    price: price,
+    type: type
+  }
+  return foodItem;
+  }
+
+  function addIngredients(item, ingredients){
+    if (!ingredients.includes(item)){
+      ingredients.push(item);
+  }
 }
 
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
+};
+
+function decreasePrice(){
+  return .90*(initialPrice);
+}
+
+function createRecipe(title, ingredients, menuItemType){
+  return recipe = {
+    title:title,
+    ingredients:ingredients,
+    type:menuItemType
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe,
 }
-
-
